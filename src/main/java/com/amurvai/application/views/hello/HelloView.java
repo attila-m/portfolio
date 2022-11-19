ef32d103beced787ce02218852f382758056b314
start.vaadin.com <no-reply@vaadin.com>
2022-11-19 17:27:24 +0000

    Generated project

---
+++ src/main/java/com/amurvai/application/views/hello/HelloView.java
@@ -0,0 +1,35 @@
+package com.amurvai.application.views.hello;
+
+import com.amurvai.application.views.MainLayout;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Hello")
+@Route(value = "Hello", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class HelloView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public HelloView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+        sayHello.addClickShortcut(Key.ENTER);
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
